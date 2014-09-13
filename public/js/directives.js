bb2a8022077ed0899313cbd73e8c8751f5959ae4
ayan4m1 <andrew@bulletlogic.com>
2014-09-13 16:12:39 -0400

    add line-chart directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -2,6 +2,85 @@
 
 var directiveModule = angular.module('uchiwa.directives', []);
 
+directiveModule.directive('lineChart', ['d3', function (d3) {
+  return {
+    replace: true,
+    restrict: 'E',
+    scope: {
+      data: '='
+    },
+    link: function (scope, element) {
+      var graph = d3.select(element[0])
+        .append('svg')
+        .attr('width', '100%')
+        .attr('height', '100%');
+
+      var scale = {
+        x: d3.time.scale().range([0, element.find('svg').width()]),
+        y: d3.scale.linear().range([element.find('svg').height(), 0])
+      };
+
+      var line = d3.svg.line()
+        .interpolate('basis')
+        .x(function (d) { return scale.x(d.timestamp); })
+        .y(function (d) { return scale.y(d.value); });
+
+      var draw = function () {
+        var seriesKeys = Object.keys(scope.data[0]).filter(function (v, i) { return i != 0; });
+
+        // get one color for each present series
+        var colors = d3.scale.category10();
+        colors.domain(seriesKeys);
+
+        // transpose data to per-series structure
+        var seriesData = colors.domain().map(function (series) {
+          return {
+            name: series,
+            values: scope.data.map(function (v) {
+              return {
+                timestamp: v.timestamp,
+                value: +v[series]
+              }
+            })
+          };
+        });
+
+        // update scale domains
+        scale.x.domain(d3.extent(scope.data, function (v) { return v.timestamp; }));
+        scale.y.domain([
+          d3.min(seriesData, function (v) { return d3.min(v.values, function (v) { return v.value }); }),
+          d3.max(seriesData, function (v) { return d3.max(v.values, function (v) { return v.value }); })
+        ]);
+
+        // create series elements
+        var series = graph.selectAll('.series')
+          .data(seriesData)
+          .enter()
+          .append('g')
+          .attr('class', 'series');
+
+        // create lines for each series
+        var path = series.append('path')
+          .attr('class', 'line')
+          .attr('d', function (d) {
+            return line(d.values);
+          })
+          .style("stroke", function(d) { return colors(d.name); });
+      };
+
+      scope.$watch('data', function() {
+        graph.selectAll('*').remove();
+
+        if (!scope.data) {
+          return;
+        }
+
+        draw();
+      });
+    }
+  }
+}]);
+
 directiveModule.directive('bootstrapTooltip', function () {
   return {
     restrict: 'EA',
